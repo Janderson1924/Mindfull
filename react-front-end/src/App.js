417b25354871129b0793173ae2fd4ee9618a7876
Justin Anderson <justinanderson101@gmail.com>
2021-05-11 20:17:28 -0600

    defined async fucntion to bring in title and links from GNN RSS feed

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -4,26 +4,39 @@ import './App.scss';
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 import Home from "./components/home";
 
+const Parser = require('rss-parser');
+const parser = new Parser();
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      message: 'Click the button to load data!'
+      // message: 'Click the button to load data!'
+      feed: []
     }
   }
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+async componentDidMount() {
+  const feed = await parser.parseURL('https://cors-anywhere.herokuapp.com/https://www.goodnewsnetwork.org/category/news/inspiring/feed/');
+  this.setState({ feed });
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
+  feed.items.slice(-5).forEach(item => {
+    console.log(`${item.title}: ${item.link}`);
+  });
+}  
+
+fetchData = () => {
+  axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+  .then((response) => {
+    // handle success
+    console.log(response.data) // The entire response from the Rails API
+
+    console.log(response.data.message) // Just the message
+    this.setState({
+      message: response.data.message
+    });
+  }) 
+}
 
   render() {
     return (
